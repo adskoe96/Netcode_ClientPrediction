d7265863739cd1c22069904ad9661baea8bb65c2
adsk <73867345+adskoe96@users.noreply.github.com>
2024-07-17 16:22:14 +0300

    added class: TransformState
    necessary for NetworkPlayerControls

---
+++ TransformState.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+internal class TransformState : INetworkSerializable
+{
+    internal int Tick;
+    internal Vector3 Position;
+    internal Quaternion Rotation;
+    internal bool HasStartedMoving;
+    
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        if (serializer.IsReader)
+        {
+            var reader = serializer.GetFastBufferReader();
+            reader.ReadValueSafe(out Tick);
+            reader.ReadValueSafe(out Position);
+            reader.ReadValueSafe(out Rotation);
+            reader.ReadValueSafe(out HasStartedMoving);
+        }
+        else
+        {
+            var writer = serializer.GetFastBufferWriter();
+            writer.WriteValueSafe(Tick);
+            writer.WriteValueSafe(Position);
+            writer.WriteValueSafe(Rotation);
+            writer.WriteValueSafe(HasStartedMoving);
+        }
+    }
+}
